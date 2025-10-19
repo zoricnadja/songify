4cfee6c89a32e51f34e5dbf8047102a0bdc50a74
zoricnadja <zoric.n2003@gmail.com>
2025-10-19 11:26:38 +0200

    feat: Add auth module

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import awsAmplify from 'aws-amplify';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
+
+awsAmplify.Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'eu-central-1_dMyCdtFX9',
+      userPoolClientId: '3q79gv4rgi7cd4qu3a7npnl72c',
+    },
+  },
+});
