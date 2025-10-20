a956b2762c4bc8472d72c60a7530d75798ff2b26
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-21 00:56:39 +0200

    chore: terraform dynamodb

--- main.tf
+++ main.tf
@@ -17,3 +17,11 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+module "dynamodb" {
+  source = "./modules/dynamodb"
+
+  project_name   = var.project_name
+  read_capacity  = var.dynamodb_read_capacity
+  write_capacity = var.dynamodb_write_capacity
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "project_name" {
+  description = "The name of the project."
+  type        = string
+  default     = "songify"
+}
+
+variable "dynamodb_read_capacity" {
+  description = "Read capacity units for the DynamoDB table."
+  type        = number
+  default     = 1
+}
+
+variable "dynamodb_write_capacity" {
+  description = "Write capacity units for the DynamoDB table."
+  type        = number
+  default     = 1
+}
