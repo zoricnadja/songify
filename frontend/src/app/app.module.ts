4ac0e982dde5c79954410fb303533c2978b94bfb
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-21 19:54:31 +0200

    feat: add angular modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,27 @@ import { AuthModule } from './auth/auth.module';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './auth/interceptor';
 import { AuthGuard } from './auth/auth.guard';
+import { AlbumModule } from './album/album.module';
+import { ArtistModule } from './artist/artist.module';
+import { GenreModule } from './genre/genre.module';
+import { TrackModule } from './track/track.module';
+import { AdminModule } from './admin/admin.module';
+import { MaterialModule } from './infrastructure/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, AuthModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule,
+    AlbumModule,
+    ArtistModule,
+    AuthModule,
+    GenreModule,
+    LayoutModule,
+    MaterialModule,
+    TrackModule,
+  ],
   providers: [
     AuthGuard,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
