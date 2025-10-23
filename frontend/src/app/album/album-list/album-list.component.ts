16f77ccf5cce480f26b586bda23f82e43a199b97
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-23 17:28:16 +0200

    feat: add album components

---
+++ frontend/src/app/album/album-list/album-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { AlbumService } from '../album.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AlbumModalComponent } from '../album-modal/album-modal.component';
+import { Album } from '../models/album.model';
+
+@Component({
+  selector: 'app-album-list',
+  standalone: false,
+  templateUrl: './album-list.component.html',
+  styleUrl: './album-list.component.scss',
+})
+export class AlbumListComponent implements OnInit {
+  albums: Album[] = [];
+
+  constructor(
+    private albumService: AlbumService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.loadAlbums();
+  }
+
+  loadAlbums() {
+    this.albumService.getAlbums().subscribe((albums) => {
+      this.albums = albums;
+    });
+  }
+
+  openCreateModal() {
+    this.dialog
+      .open(AlbumModalComponent, {
+        width: '500px',
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          this.albumService.createAlbum(result).subscribe(
+            () => this.loadAlbums(),
+            (error) => alert(error.error.message)
+          );
+        }
+      });
+  }
+
+  openEditModal(album: Album) {
+    this.dialog
+      .open(AlbumModalComponent, {
+        width: '500px',
+        data: { album },
+      })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          this.albumService.updateAlbum(album.id, result).subscribe(
+            () => this.loadAlbums(),
+            (error) => alert(error.error.message)
+          );
+        }
+      });
+  }
+
+  deleteAlbum(album: Album) {
+    if (confirm('Are you sure you want to delete this album?')) {
+      this.albumService.deleteAlbum(album.id).subscribe(
+        () => this.loadAlbums(),
+        (error) => alert(error.error.message)
+      );
+    }
+  }
+}
--- frontend/src/app/album/album.module.ts
+++ frontend/src/app/album/album.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { AlbumModalComponent } from './album-modal/album-modal.component';
+import { MaterialModule } from '../infrastructure/material/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [AlbumListComponent, AlbumModalComponent],
+  imports: [CommonModule, FormsModule, MaterialModule],
+  exports: [AlbumListComponent],
 })
 export class AlbumModule {}
