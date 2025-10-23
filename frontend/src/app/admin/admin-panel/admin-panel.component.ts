16f505fc17e2a47d39c64536831d9e67796348bb
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-23 12:07:17 +0200

    feat: add admin panel component

---
+++ frontend/src/app/admin/admin-panel/admin-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-panel',
+  standalone: false,
+  templateUrl: './admin-panel.component.html',
+  styleUrl: './admin-panel.component.scss'
+})
+export class AdminPanelComponent {
+
+}
--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { GenreModule } from '../genre/genre.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [AdminPanelComponent],
+  imports: [CommonModule, GenreModule],
 })
 export class AdminModule {}
