6a2e666ca7fe4f4d26fd4a231bd83c6bad48a454
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-23 12:07:30 +0200

    feat: add admin panel route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './layout/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminPanelComponent },
 ];
 
 @NgModule({
