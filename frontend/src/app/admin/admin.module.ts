8fd2914d09b2a3e5c10452fbc56a0daeca1f1fc0
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-24 00:04:28 +0200

    feat: update admin panel

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { GenreModule } from '../genre/genre.module';
 import { ArtistModule } from '../artist/artist.module';
 import { AlbumModule } from '../album/album.module';
+import { TrackModule } from '../track/track.module';
 
 @NgModule({
   declarations: [AdminPanelComponent],
-  imports: [CommonModule, GenreModule, ArtistModule, AlbumModule],
+  imports: [CommonModule, GenreModule, ArtistModule, AlbumModule, TrackModule],
 })
 export class AdminModule {}
