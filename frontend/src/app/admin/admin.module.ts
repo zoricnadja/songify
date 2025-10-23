fe92f72df9fe3c04bf6e9f0a4d54042cf1331fcf
DamjanVincic <damjanvincic2003@gmail.com>
2025-10-23 15:24:01 +0200

    feat: update admin panel

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { GenreModule } from '../genre/genre.module';
+import { ArtistModule } from '../artist/artist.module';
 
 @NgModule({
   declarations: [AdminPanelComponent],
-  imports: [CommonModule, GenreModule],
+  imports: [CommonModule, GenreModule, ArtistModule],
 })
 export class AdminModule {}
