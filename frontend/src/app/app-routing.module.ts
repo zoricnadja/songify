305c723f4e4b68fc990fe02356ce3780b6ebf463
zoricnadja <zoric.n2003@gmail.com>
2025-10-24 04:08:23 +0200

    feat: Add authguards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,25 @@ import { HomeComponent } from './layout/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { SubscriptionsComponent } from './subscription/subscriptions/subscriptions.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminPanelComponent },
-  { path: 'subscriptions', component: SubscriptionsComponent },
+  {
+    path: 'admin',
+    component: AdminPanelComponent,
+    canActivate: [AuthGuard],
+    data: { role: ['admin'] },
+  },
+  {
+    path: 'subscriptions',
+    component: SubscriptionsComponent,
+    canActivate: [AuthGuard],
+    data: { role: ['user'] },
+  },
 ];
 
 @NgModule({
